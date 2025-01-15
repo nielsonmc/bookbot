44402dfdedde54230df6dac435961b209a17cf9b
nielsonmc <nielsonmc@gmail.com>
2025-01-15 14:11:20 -0700

    add main function to open file, read contents and then print contents

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+def main():
+    path_to_file = "books/frankenstein.txt"
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        print(file_contents)
+
+
+main()
