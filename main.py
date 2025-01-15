e057fbac9ef03d2fcdbb3c77927179004bb744f5
nielsonmc <nielsonmc@gmail.com>
2025-01-15 16:29:24 -0700

    Add function to count characters and return in a dictionary

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     path_to_file = "books/frankenstein.txt"
     file_contents = get_book_text(path_to_file)
     word_count = get_word_count(file_contents)
+    character_count = get_character_count(file_contents)
     print(f"Document has {word_count} words")
+    print(character_count)
 
 
 def get_book_text(path):
@@ -13,6 +15,17 @@ def get_book_text(path):
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+
+def get_character_count(text):
+    lower_text = text.lower()
+    #print(lower_text)
+    char_count = {}
+    for char in lower_text:
+        print(char)
+        char_count[char] = lower_text.count(char)
+        #print(char_count)
+    return char_count
 
 
 main()
