e1f337b18eb6419465116b744c414bfecd3ad05b
nielsonmc <nielsonmc@gmail.com>
2025-01-15 16:19:43 -0700

    Moved getting file text to a function, added new function to get a word count

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 def main():
     path_to_file = "books/frankenstein.txt"
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        print(file_contents)
+    file_contents = get_book_text(path_to_file)
+    word_count = get_word_count(file_contents)
+    print(f"Document has {word_count} words")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def get_word_count(text):
+    words = text.split()
+    return len(words)
 
 
 main()
