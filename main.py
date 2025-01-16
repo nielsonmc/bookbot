4638e44abf87b5cd470daf8b429f848df89bf9d1
nielsonmc <nielsonmc@gmail.com>
2025-01-15 19:42:10 -0700

    Added functions to convert to a list of dictionaires and sort by the number of occurrences

--- main.py
+++ main.py
@@ -3,48 +3,52 @@ def main():
     file_contents = get_book_text(path_to_file)
     word_count = get_word_count(file_contents)
     character_count = get_character_count(file_contents)
-    #char_report = get_character_sorted_dict(char_count)
-    #print(f"Document has {word_count} words")
-    print(character_count)
+    char_report = get_character_sorted_dict(character_count)
+    #print(character_count)
+    #print(char_report)
     print(f"--- Begin report of {path_to_file} ---")
+    #print(f"Document has {word_count} words")
     print(f"{word_count} words found in the document")
     print()
     
-
-
+    for item in char_report:
+        print(f"The '{item['char']}' character was found {item['num']} times")
+    
+    print("--- End report ---")
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-
 def get_word_count(text):
     words = text.split()
+    #print(words)
     return len(words)
-
 
 def get_character_count(text):
     char_count = {}
-    '''
-    lower_text = text.lower()
-    print(lower_text)
-    for char in lower_text:
-        char_count[char] = lower_text.count(char)
-        '''
     for char in text:
         #print(char)
         lowered = char.lower()
         #print(lowered)
-        if lowered in char_count:
-            char_count[lowered] += 1
-        else:
-            char_count[lowered] = 1
+        if lowered.isalpha():
+            if lowered in char_count:
+                char_count[lowered] += 1
+            else:
+                char_count[lowered] = 1
         #print(char_count)
     return char_count
 
+def sort_on(dict):
+    return dict["num"]
 
-#def get_character_sorted_dict(text):
+def get_character_sorted_dict(character_count):
+    sorted_list = []
+    for item in character_count:
+        sorted_list.append({"char": item, "num": character_count[item]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 
 main()
