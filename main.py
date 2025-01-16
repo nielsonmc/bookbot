b07741a4c7d445b52ab69b0c2ebb8ac69a0b39ea
nielsonmc <nielsonmc@gmail.com>
2025-01-15 17:35:40 -0700

    Change lower conversion and char_count to more efficient method

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ def main():
     file_contents = get_book_text(path_to_file)
     word_count = get_word_count(file_contents)
     character_count = get_character_count(file_contents)
-    print(f"Document has {word_count} words")
+    #char_report = get_character_sorted_dict(char_count)
+    #print(f"Document has {word_count} words")
     print(character_count)
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{word_count} words found in the document")
+    print()
+    
+
+
 
 
 def get_book_text(path):
@@ -18,14 +25,26 @@ def get_word_count(text):
 
 
 def get_character_count(text):
-    lower_text = text.lower()
-    #print(lower_text)
     char_count = {}
+    '''
+    lower_text = text.lower()
+    print(lower_text)
     for char in lower_text:
-        print(char)
         char_count[char] = lower_text.count(char)
+        '''
+    for char in text:
+        #print(char)
+        lowered = char.lower()
+        #print(lowered)
+        if lowered in char_count:
+            char_count[lowered] += 1
+        else:
+            char_count[lowered] = 1
         #print(char_count)
     return char_count
+
+
+#def get_character_sorted_dict(text):
 
 
 main()
